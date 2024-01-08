593b6bcf7f3121af43eb02d8cdbe2744b289e262
chororinko <147678517+chororinko@users.noreply.github.com>
2024-01-08 14:12:34 +0000

    public/top

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,11 @@
 class SearchesController < ApplicationController
+
+  def search
+		@content = params[:content]
+    if @content == "Snack"
+      @snacks = Snack.looks(@content, @search)
+      render '/searches/search_result'
+    end
+  end
+
 end
--- app/models/snack.rb
+++ app/models/snack.rb
@@ -1,2 +1,24 @@
 class Snack < ApplicationRecord
+
+  has_one_attached :image
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+      image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def self.looks(content, search)
+    if search == "perfect_match"
+      Snack.where("title LIKE?", "#{content}")
+    elsif search == "forward_match"
+      Snack.where("title LIKE?", "#{content}%")
+    elsif search == "backward_match"
+      Snack.where("title LIKE?", "%#{content}")
+    else
+      Snack.where("title LIKE?", "%#{content}%")
+    end
+  end
 end
