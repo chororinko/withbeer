302b38a47b692394a78fb4590d5df41c41226f6f
chororinko <147678517+chororinko@users.noreply.github.com>
2024-01-19 10:48:08 +0000

    layout

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ class SearchesController < ApplicationController
   def search
     @tags = Tag.all
     @word = params[:word]
-    @word == "Snack"
+    @word == Snack
     @snacks = Snack.looks(@word).order(created_at: :desc).page(params[:page]).per(2)
     render 'searches/search'
   end
