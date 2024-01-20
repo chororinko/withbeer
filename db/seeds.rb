63c2f9d4bfc6bc714cc59bea2bf671f92b48337d
chororinko <147678517+chororinko@users.noreply.github.com>
2024-01-20 14:42:47 +0000

    fix admin's email,password

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create!(
-   email: 'withbeer@withbeer',
-   password: 'withbeer'
+   email: 'withbeer1@withbeer1',
+   password: 'withbeer1'
 )
 
