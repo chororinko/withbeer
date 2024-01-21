7eddad4c23b6b1502f7dc6779db2a19a1e1e4fb4
chororinko <147678517+chororinko@users.noreply.github.com>
2024-01-21 01:10:10 +0000

    fix top's layout, add time_zone

--- app/controllers/admin/snack_comments_controller.rb
+++ app/controllers/admin/snack_comments_controller.rb
@@ -3,6 +3,7 @@ class Admin::SnackCommentsController < ApplicationController
   def destroy
     @snack = Snack.find(params[:snack_id])
     @snack_comment = @snack.snack_comments.find(params[:id]).destroy
+    flash[:notice] = 'コメントを削除しました。'
     redirect_to request.referer
   end
 
