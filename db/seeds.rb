4ce1bedc19f6089d4521f89885f5f2a6bd1aee70
chororinko <147678517+chororinko@users.noreply.github.com>
2024-02-14 01:14:09 +0000

    modified: add test-data

--- db/seeds.rb
+++ db/seeds.rb
@@ -125,8 +125,76 @@ A（ニンニクチューブ：3センチ
   snack.tags << Tag.find_or_create_by!(name: "和食")
 end
 
+Snack.find_or_create_by!(title: "定番！生姜焼き") do |snack|
+  snack.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-snack6.jpg"), filename:"sample-snack6.jpg")
+  snack.introduction = "お肉＝赤ワインと思われがちですが、実は白ワインにもとっても合います！ぜひ、白ワインのお供にお試しください。"
+  snack.customer = 冬子
+  snack.ingredients =
+"（2人前）
+豚ロース（生姜焼き用）：8枚
+市販の生姜焼きのタレ：100g
+
+【付け合わせ】
+玉ねぎ：1個
+もやし：200g
+舞茸：1パック
+
+【下準備】
+豚ロース：食べやすい大きさに切る
+玉ねぎ：1センチ幅にくし切りする
+舞茸：食べやすい大きさに分ける"
+  snack.process =
+"1. 鍋にサラダ油小さじ1を入れ、豚ロースを焼きます。
+2. 焼き色がついたらタレ入れ絡めます。
+3. 焦げ目がつくぐらいまで焼いたら、お肉をお皿に取り出します。
+4. 付け合わせの野菜を鍋に入れ、残ったタレで炒めます。
+5. 野菜にタレが染み込み、しんなりしたらお皿に盛り、出来上がりです。
+6. お好みでマヨネーズと七味をかけても美味しいです。"
+  snack.sake = Sake.find(3)
+  snack.tags << Tag.find_or_create_by!(name: "和食")
+  snack.tags << Tag.find_or_create_by!(name: "こってり")
+  snack.tags << Tag.find_or_create_by!(name: "簡単")
+end
+
+Snack.find_or_create_by!(title: "ピーマンの肉詰め") do |snack|
+  snack.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-snack7.jpg"), filename:"sample-snack7.jpg")
+  snack.introduction =
+  "みんな大好きピーマンの肉詰め！！
+お酒のおつまみとしても最高に合います！！
+お酒も肉詰めもどちらも進むこと間違いなし！！"
+  snack.customer = 次郎
+  snack.ingredients =
+"（2人前）
+ピーマン：4個
+合い挽き肉：150g
+片栗粉：大さじ1
+サラダ油：大さじ1/2
+
+A（玉ねぎ：1/4個
+　　片栗粉・酒：各小さじ2
+　　塩コショウ：各少々）
+
+B（ケチャップ：大さじ1
+　　水・ウスターソース：各大さじ2）
+
+【下準備】
+玉ねぎ：みじん切りにする
+ピーマン：縦半分に切って種とわたをとる"
+  snack.process =
+"1. ピーマンに片栗粉をふります。
+2. ボウルでひき肉とAを混ぜて肉だねを作り、ピーマンの数に合わせて分け、ピーマンにぎゅうぎゅうにつめます。
+【POINT】
+焼くときに肉だねが縮み、ピーマンから出てしまうこともあるので、ぎゅうぎゅうに詰めておいてください。
+3. フライパンにサラダ油を入れて中火で熱し、2の肉だねを下にした状態で焼きます。焼き色がついたら裏返し、ふたをして弱火で4~5分蒸し焼きにします。
+4. 余分な油を拭き取り、Bを加えて煮絡めます。
+5. お皿に盛り完成です。"
+  snack.sake = Sake.find(2)
+  snack.tags << Tag.find_or_create_by!(name: "洋食")
+  snack.tags << Tag.find_or_create_by!(name: "こってり")
+end
+
 Snack.find_or_create_by!(title: "舞茸とチーズのベーコン巻き") do |snack|
-  snack.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-snack5.jpg"), filename:"sample-snack5.jpg")
+  snack.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-snack4.jpg"), filename:"sample-snack4.jpg")
   snack.introduction = "定番のベーコン＆チーズに、舞茸を加えた”THEおつまみ”な逸品です。ビールのお供にお試しください。"
   snack.customer = nil
   snack.ingredients =
@@ -149,4 +217,39 @@ Snack.find_or_create_by!(title: "舞茸とチーズのベーコン巻き") do |s
   snack.sake = Sake.find(1)
   snack.tags << Tag.find_or_create_by!(name: "和食")
   snack.tags << Tag.find_or_create_by!(name: "簡単")
+end
+
+Snack.find_or_create_by!(title: "肉じゃが") do |snack|
+  snack.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-snack5.jpg"), filename:"sample-snack5.jpg")
+  snack.introduction = "定番のおかずメニューですが、日本酒との相性バッチリです！！ぜひ、日本酒のお供にお試しください。"
+  snack.customer = nil
+  snack.ingredients =
+"（2人前）
+じゃがいも：3個
+玉ねぎ：1個
+豚バラ：250g
+白滝：1袋
+冷凍いんげん：5本
+
+A（すき焼きのタレ：150ml
+水：500ml
+砂糖：大さじ2
+醤油：50ml）
+
+【下準備】
+じゃがいも：四つ切りにする
+玉ねぎ：1センチ幅にくし切りする
+豚バラ：食べやすい大きさに切る
+白滝：食べやすい大きさに切る
+いんげん：3センチくらいに切る"
+  snack.process =
+"1. 鍋にサラダ油小さじ1を入れ、豚バラを炒め、色がついてきたらじゃがいも、玉ねぎ、白滝を入れて炒めます。
+2. 1にAを加えます。
+3. 蓋をして中火で30分煮ます。（都度あくを取る）
+4. いんげんを入れてさらに5分ほど煮ます。
+5. じゃがいもに箸を刺し、柔らかくなったと感じたらお皿に盛って出来上がりです。
+6. お好みで七味をかけても美味しいです。"
+  snack.sake = Sake.find(4)
+  snack.tags << Tag.find_or_create_by!(name: "和食")
+  snack.tags << Tag.find_or_create_by!(name: "こってり")
 end
