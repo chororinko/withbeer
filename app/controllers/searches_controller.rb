587bd95ad2a0329f4b1a457e33ad7cbd04d70f29
chororinko <147678517+chororinko@users.noreply.github.com>
2024-02-14 13:31:14 +0000

    modified: sorted keyword-snacks-index

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,14 +3,13 @@ class SearchesController < ApplicationController
   def search
     @tags = Tag.all
     @word = params[:word]
-    @word == Snack
     @snacks = Snack.looks(@word).order(created_at: :desc).page(params[:page])
     if params[:latest]
-      @snacks = @word.snacks.latest.page(params[:page])
+      @snacks = Snack.looks(@word).latest.page(params[:page])
     elsif params[:old]
-      @snacks = @word.snacks.old.page(params[:page])
+      @snacks = Snack.looks(@word).old.page(params[:page])
     elsif params[:most_favorited]
-      @snacks = @word.snacks.most_favorited.page(params[:page])
+      @snacks = Snack.looks(@word).most_favorited.page(params[:page])
     end
     render 'searches/search'
   end
