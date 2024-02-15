a3a52c05ccd855b43f545072714fb328b8c210b2
chororinko <147678517+chororinko@users.noreply.github.com>
2024-02-15 07:35:18 +0000

    modified: add most-favorited-snack

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,8 +4,8 @@ class Public::HomesController < ApplicationController
     @sakes = Sake.all
     @tags = Tag.all
     @snack = Snack.where(customer_id: nil).last
-    @most_favorited = Snack.find(Favorite.where(snack_id: params[:id]).group(:snack_id).order('COUNT(snack_id) desc').limit(1).pluck(:snack_id))
-    # limit(1)・・・orderでいいねが多い順に並べ替えられたものの最初の1つを取得=>いいねが最も多い投稿を取得
+    @most_favorited = Snack.left_joins(:favorites).group('snacks.id').order('COUNT(favorites.id) DESC').first
+    #orderでいいねが多い順に並べ替えられたものの最初の1つ(first)を取得=>いいねが最も多い投稿を取得
   end
 
 end
